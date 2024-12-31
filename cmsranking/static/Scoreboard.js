9f7575930653548713f83169343ac8ff36cf7026
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-12-31 18:32:50 +0330

    Debug ranking

--- cmsranking/static/Scoreboard.js
+++ cmsranking/static/Scoreboard.js
@@ -254,7 +254,9 @@ var Scoreboard = new function () {
 
         if (user['team']) {
             result += " \
-    <td class=\"team\"><img src=\"" + Config.get_flag_url(user["team"]) + "\" title=\"" + DataStore.teams[user["team"]]["name"] + "\" /></td>";
+    // <td class=\"team\"><img src=\"" + Config.get_flag_url(user["team"]) + "\" title=\"" + DataStore.teams[user["team"]]["name"] + "\" /></td>";
+            result += " \
+    <td class=\"team\">" + user['team'] + "</td>";
         } else {
             result += " \
     <td class=\"team\"></td>";
