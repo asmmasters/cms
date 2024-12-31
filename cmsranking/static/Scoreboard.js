07dd458ec278f6862d6f440349aa8ee5edfb0c70
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-12-31 17:20:54 +0330

    Remove name from scoreboard

--- cmsranking/static/Scoreboard.js
+++ cmsranking/static/Scoreboard.js
@@ -188,8 +188,6 @@ var Scoreboard = new function () {
 <tr> \
     <th class=\"sel\"></th> \
     <th class=\"rank\">Rank</th> \
-    <th colspan=\"10\" class=\"f_name\">First Name</th> \
-    <th colspan=\"10\" class=\"l_name\">Last Name</th> \
     <th class=\"team\">Team</th>";
 
         var contests = DataStore.contest_list;
